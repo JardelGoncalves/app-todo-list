7588b2fe6449d24f02f6dfe51d2663c02588b795
Jardel <jardelgoncalves1996@gmail.com>
2019-07-01 00:51:34 -0300

    interfaces padronizadas para retorno de respostas para o controller

---
+++ backend/src/helpers/response-default.js
@@ -0,0 +1,15 @@
+import HttpStatus from 'http-status'
+const successResponse = (data, status = HttpStatus.OK) => ({
+  data: data || {},
+  status
+})
+
+const errorResponse = (message, status = HttpStatus.BAD_REQUEST) => ({
+  data: { error: message },
+  status
+})
+
+export {
+  successResponse,
+  errorResponse
+}
