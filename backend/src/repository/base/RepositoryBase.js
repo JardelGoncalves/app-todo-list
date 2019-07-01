20ef291f0012f8e4ba4d3b53fc13b0168db141d3
Jardel <jardelgoncalves1996@gmail.com>
2019-07-01 00:49:49 -0300

    add RepositoryBase com os métodos comuns para CRUD

---
+++ backend/src/repository/base/RepositoryBase.js
@@ -0,0 +1,67 @@
+import HttpStatus from 'http-status'
+import { successResponse, errorResponse } from '../../helpers/response-default'
+
+export default class ReposittoryBase {
+  constructor (Model) {
+    this.Model = Model
+  }
+
+  getAll () {
+    return this.Model.findAll()
+      .then(result => successResponse(result))
+      .catch(() => errorResponse('Ocorreu um erro inesperado. Contate o administrador!', HttpStatus.INTERNAL_SERVER_ERROR))
+  }
+
+  getById (params, Validator) {
+    if (!Validator.hasError()) {
+      return this.Model.findOne({ where: params })
+        .then(result => successResponse(result))
+        .catch(() => errorResponse('Ocorreu um erro inesperado. Contate o administrador!', HttpStatus.INTERNAL_SERVER_ERROR))
+    }
+
+    return new Promise((resolve, reject) => {
+      resolve(errorResponse(Validator.errors))
+    })
+  }
+
+  create (data, Validator) {
+    if (!Validator.hasError()) {
+      return this.Model.create(data)
+        .then(result => successResponse(result, HttpStatus.CREATED))
+        .catch(() => errorResponse('Ocorreu um erro inesperado. Contate o administrador!', HttpStatus.INTERNAL_SERVER_ERROR))
+    }
+
+    return new Promise((resolve, reject) => {
+      resolve(errorResponse(Validator.errors))
+    })
+  }
+
+  update (params, data, Validator) {
+    if (!Validator.hasError()) {
+      return this.Model.update(data, { where: params })
+        .then(() => this.Model.findOne({ where: params })
+          .then(result => successResponse(result))
+        )
+        .catch(err => {
+          console.log(err)
+          return errorResponse('Ocorreu um erro inesperado. Contate o administrador!', HttpStatus.INTERNAL_SERVER_ERROR)
+        })
+    }
+
+    return new Promise((resolve, reject) => {
+      resolve(errorResponse(Validator.errors))
+    })
+  }
+
+  delete (params, Validator) {
+    if (!Validator.hasError()) {
+      return this.Model.destroy({ where: params })
+        .then(result => successResponse(result, HttpStatus.NO_CONTENT))
+        .catch(() => errorResponse('Ocorreu um erro inesperado. Contate o administrador!', HttpStatus.INTERNAL_SERVER_ERROR))
+    }
+
+    return new Promise((resolve, reject) => {
+      resolve(errorResponse(Validator.errors))
+    })
+  }
+}
