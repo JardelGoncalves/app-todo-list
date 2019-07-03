d0601cdbc162d22509a8cdb0214f42449c031683
Jardel <jardelgoncalves1996@gmail.com>
2019-07-03 13:04:00 -0300

    adicionando o user_id para as requisições

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -7,10 +7,11 @@ export default (app) => {
   app.route('/tasks')
     .all(app.auth.authenticate())
     .get((req, res) => {
-      return _taskController.getAll()
+      return _taskController.getAll({ user_id: app.userId })
         .then(result => res.status(result.status).json(result.data))
     })
     .post((req, res) => {
+      req.body.user_id = app.userId
       return _taskController.create(req.body)
         .then(result => res.status(result.status).json(result.data))
     })
@@ -18,15 +19,15 @@ export default (app) => {
   app.route('/tasks/:id')
     .all(app.auth.authenticate())
     .get((req, res) => {
-      return _taskController.getById({ id: req.params.id })
+      return _taskController.getById({ id: req.params.id, user_id: app.userId })
         .then(result => res.status(result.status).json(result.data))
     })
     .put((req, res) => {
-      return _taskController.update({ id: req.params.id }, req.body)
+      return _taskController.update({ id: req.params.id, user_id: app.userId }, req.body)
         .then(result => res.status(result.status).json(result.data))
     })
     .delete((req, res) => {
-      return _taskController.delete({ id: req.params.id })
+      return _taskController.delete({ id: req.params.id, user_id: app.userId })
         .then(result => res.status(result.status).send())
     })
 }
