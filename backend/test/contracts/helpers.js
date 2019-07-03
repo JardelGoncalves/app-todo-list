602a6e8acbd78a7320cc9a634c2383ecbf5c0702
Jardel <jardelgoncalves1996@gmail.com>
2019-07-02 23:58:02 -0300

    add novas config para o ambiente de testes de contrato

--- backend/test/contracts/helpers.js
+++ backend/test/contracts/helpers.js
@@ -2,10 +2,16 @@ import chai from 'chai'
 import supertest from 'supertest'
 import Joi from '@hapi/joi'
 import JoiAssert from 'joi-assert'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 import app from '../../src/app'
+
+dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env' : '.env.test' })
 
 global.app = app
 global.request = supertest(app)
 global.expect = chai.expect
 global.Joi = Joi
 global.JoiAssert = JoiAssert
+global.jwt = jwt
+global.secretKey = process.env.APP_SECRET
