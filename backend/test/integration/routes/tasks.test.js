4749c8df64d78a2b747a86be9604a303b7851f9b
Jardel <jardelgoncalves1996@gmail.com>
2019-07-02 23:52:06 -0300

    adicionando o token no cabeçalho das requisições

--- backend/test/integration/routes/tasks.test.js
+++ backend/test/integration/routes/tasks.test.js
@@ -1,6 +1,7 @@
 describe('Routes Tasks', () => {
 
   const Task = app.datasource.models.Tasks
+  const User = app.datasource.models.Users
   const TaskDefault = {
     id: 1,
     title: 'title default',
@@ -9,18 +10,33 @@ describe('Routes Tasks', () => {
     completed: false
   }
 
+  let token
+
   beforeEach(done => {
     Task.destroy({ where: {} })
-      .then(() => Task.create(TaskDefault)
-        .then(() => {
-          done()
+      .then(() => {
+        User.destroy({ where: {} })
+          .then(() => User.create({
+            id: 1,
+            name: 'jardel',
+            email: 'jardel@admin.com',
+            password: 'admin'
         }))
+          .then(user => {
+              Task.create(TaskDefault)
+              .then(() => {
+                token = jwt.sign({ id: user.id }, secretKey)
+                done()
+              })
+          })
+      })
   })
 
   describe('Route GET /tasks', () => {
     it('should return a task lists', done => {
       request
         .get('/tasks')
+        .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           expect(res.body[0].id).to.be.eql(TaskDefault.id)
           expect(res.body[0].title).to.be.eql(TaskDefault.title)
@@ -36,6 +52,7 @@ describe('Routes Tasks', () => {
     it('should return a task', done => {
       request
         .get('/tasks/1')
+        .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           expect(res.body.id).to.be.eql(TaskDefault.id)
           expect(res.body.title).to.be.eql(TaskDefault.title)
@@ -58,6 +75,7 @@ describe('Routes Tasks', () => {
       }
       request
         .post('/tasks')
+        .set('Authorization', `Bearer ${token}`)
         .send(newTask)
         .end((err, res) => {
           expect(res.body.id).to.be.eql(newTask.id)
@@ -79,6 +97,7 @@ describe('Routes Tasks', () => {
       }
       request
         .put('/tasks/1')
+        .set('Authorization', `Bearer ${token}`)
         .send(updateTask)
         .end((err, res) => {
           expect(res.body.id).to.be.eql(TaskDefault.id)
@@ -95,6 +114,7 @@ describe('Routes Tasks', () => {
     it('should delete a task', done => {
       request
         .delete('/tasks/1')
+        .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           expect(res.status).to.be.eql(204)
           done(err)
