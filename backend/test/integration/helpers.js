5b14b10afe8ec79ecf6adb4723187378d414da83
Jardel <jardelgoncalves1996@gmail.com>
2019-07-02 23:51:42 -0300

    add novas configurações para os testes de integração (para autenticação)

--- backend/test/integration/helpers.js
+++ backend/test/integration/helpers.js
@@ -1,7 +1,14 @@
 import chai from 'chai'
 import supertest from 'supertest'
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 import app from '../../src/app'
+
+dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env' : '.env.test' })
+
 
 global.app = app
 global.request = supertest(app)
 global.expect = chai.expect
+global.jwt = jwt
+global.secretKey = process.env.APP_SECRET
